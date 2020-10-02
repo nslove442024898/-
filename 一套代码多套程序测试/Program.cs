e66af0d0f85b8783513bddbbbb11652580521235
胜 南 <nslove442024898@live.com>
2020-10-02 10:20:27 +0800

    添加项目文件。

---
+++ 一套代码多套程序测试/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 一套代码多套程序测试
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if ACAD||DEBUG
+            global::System.Console.WriteLine("This is AutoCAD!!!");
+#elif BCAD
+            global::System.Console.WriteLine("This is BricsCAD!!!");
+#else
+            global::System.Console.WriteLine("This is GstarCAD!!!");
+#endif
+            Console.ReadKey();
+        }
+    }
+}
